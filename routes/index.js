fef5b8f11c2929c4ac0abeb74aef446c7eaf7f3b
ojo19001@byui.edu <ojo19001@byui.edu>
2022-11-21 17:33:48 +0100

    Practcing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const routes = require("express").Router();
 
 routes.get("/", (req, res) => {
-  res.send("Rufus Olajide Ojo");
+  let contacts = {
+    name: "Ojo RUFUS Olajide",
+    age: 34,
+  };
+  res.send(contacts.name);
 });
 
 module.exports = routes;
